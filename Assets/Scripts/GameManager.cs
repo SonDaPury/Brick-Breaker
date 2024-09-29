7c745346626d4b1545918d6e29cf3f49a119873b
Pury <sondongxuan1205@gmail.com>
2024-09-29 19:32:52 +0700

    add level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public List<Transform> spawnPoints;
+    public List<Transform> spawnTargets;
     public List<GameObject> bricksInScene;
     public List<GameObject> ballsInScene;
     public GameObject triangleBrick;
@@ -24,65 +25,58 @@ public class GameManager : MonoBehaviour
     {
         level = 1;
 
-        foreach (Transform spawnPoint in spawnPoints)
-        {
-            int brickToCreate = Random.Range(0, 3);
+        spawnTargets = GetRandomSpawnPoint(spawnPoints, 2);
 
-            if (brickToCreate == 0)
-            {
-                bricksInScene.Add(
-                    Instantiate(squareBrick, spawnPoint.position, Quaternion.identity)
-                );
-            }
-            else if (brickToCreate == 1)
-            {
-                bricksInScene.Add(
-                    Instantiate(triangleBrick, spawnPoint.position, Quaternion.identity)
-                );
-            }
-            else if (brickToCreate == 2 && numberOfExtraBallsInRow == 0)
+        bool hasExtraBall = false;
+
+        foreach (Transform spawnPoint in spawnTargets)
+        {
+            int brickToCreate = Random.Range(0, 2);
+
+            if (!hasExtraBall)
             {
                 bricksInScene.Add(
                     Instantiate(extraBallPowerup, spawnPoint.position, Quaternion.identity)
                 );
-
-                numberOfExtraBallsInRow++;
+                hasExtraBall = true;
+            }
+            else
+            {
+                if (brickToCreate == 0)
+                {
+                    bricksInScene.Add(
+                        Instantiate(squareBrick, spawnPoint.position, Quaternion.identity)
+                    );
+                }
+                else if (brickToCreate == 1)
+                {
+                    bricksInScene.Add(
+                        Instantiate(triangleBrick, spawnPoint.position, Quaternion.identity)
+                    );
+                }
             }
         }
-
-        numberOfExtraBallsInRow = 0;
     }
 
     public void PlaceBricks()
     {
         level++;
-        foreach (Transform spawnPoint in spawnPoints)
-        {
-            int brickToCreate = Random.Range(0, 3);
 
-            if (brickToCreate == 0)
-            {
-                GameObject brick = objectPool.GetPooledObject("SquareBrick");
-                bricksInScene.Add(brick);
-                if (brick != null)
-                {
-                    brick.transform.position = spawnPoint.position;
-                    brick.transform.rotation = Quaternion.identity;
-                    brick.SetActive(true);
-                }
-            }
-            else if (brickToCreate == 1)
-            {
-                GameObject brick = objectPool.GetPooledObject("TriangleBrick");
-                bricksInScene.Add(brick);
-                if (brick != null)
-                {
-                    brick.transform.position = spawnPoint.position;
-                    brick.transform.rotation = Quaternion.identity;
-                    brick.SetActive(true);
-                }
-            }
-            else if (brickToCreate == 2 && numberOfExtraBallsInRow == 0)
+        int targetsCount =
+            level < 10 ? 3
+            : level < 50 ? 4
+            : level < 100 ? 5
+            : 6;
+
+        spawnTargets = GetRandomSpawnPoint(spawnPoints, targetsCount);
+
+        bool hasExtraBall = false;
+
+        foreach (Transform spawnPoint in spawnTargets)
+        {
+            int brickToCreate = Random.Range(0, 2);
+
+            if (!hasExtraBall)
             {
                 GameObject ball = objectPool.GetPooledObject("ExtraBallPowerup");
                 bricksInScene.Add(ball);
@@ -91,10 +85,56 @@ public class GameManager : MonoBehaviour
                     ball.transform.position = spawnPoint.position;
                     ball.transform.rotation = Quaternion.identity;
                     ball.SetActive(true);
+                    hasExtraBall = true;
                 }
-                numberOfExtraBallsInRow++;
+            }
+            else
+            {
+                if (brickToCreate == 0)
+                {
+                    GameObject brick = objectPool.GetPooledObject("SquareBrick");
+                    bricksInScene.Add(brick);
+                    if (brick != null)
+                    {
+                        brick.transform.position = spawnPoint.position;
+                        brick.transform.rotation = Quaternion.identity;
+                        brick.SetActive(true);
+                    }
+                }
+                else if (brickToCreate == 1)
+                {
+                    GameObject brick = objectPool.GetPooledObject("TriangleBrick");
+                    bricksInScene.Add(brick);
+                    if (brick != null)
+                    {
+                        brick.transform.position = spawnPoint.position;
+                        brick.transform.rotation = Quaternion.identity;
+                        brick.SetActive(true);
+                    }
+                }
             }
         }
+
         numberOfExtraBallsInRow = 0;
+    }
+
+    public List<Transform> GetRandomSpawnPoint(List<Transform> spawnPoints, int count)
+    {
+        List<Transform> randomSpawnPoints = new();
+
+        count = Mathf.Min(count, spawnPoints.Count);
+
+        while (randomSpawnPoints.Count < count)
+        {
+            int randomIndex = Random.Range(0, spawnPoints.Count);
+            Transform randomSpawnPoint = spawnPoints[randomIndex];
+
+            if (!randomSpawnPoints.Contains(randomSpawnPoint))
+            {
+                randomSpawnPoints.Add(randomSpawnPoint);
+            }
+        }
+
+        return randomSpawnPoints;
     }
 }
